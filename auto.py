5fae7825cd22ae94cce73475f8d052792126831e
omvhedde <prasad8598@gmail.com>
2022-09-11 22:03:18 +0530

    Listed all the destination paths and file formats required for the automation

---
+++ auto.py
@@ -0,0 +1,14 @@
+import os
+
+sourceDir = '/home/omprasad/Downloads'
+docDir ='/home/omprasad/Documents'
+vidDir = '/home/omprasad/Videos'
+imgDir = '/home/omprasad/Pictures/Images'
+pkgDir = '/home/omprasad/Packages'
+archDir = '/home/omprasad/Zip'
+
+imgFormats = ['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff', '.bmp', '.eps']
+vidFormats = ['.mp4', '.mkv', '.wmv', '.mov', '.avi', '.webm']
+docFormats = ['.doc','.pdf', '.ppt', '.pptx', '.txt', '.srt']
+pkgFormats = ['.deb', '.pkg', '.whl']
+archFormats = ['.zip', '.xz', '.tar', '']
